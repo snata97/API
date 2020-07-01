b40ea6c8bbdc7953ca2bd4ff952192b8450646be
Tasha <39377219+snata97@users.noreply.github.com>
2020-07-01 12:36:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.options('*', cors({credentials: true, origin: true}));
 app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
+//    res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
     res.setHeader("Content-Type", "application/json");
