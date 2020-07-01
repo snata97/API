ddbac80133c1293df46106c98de58d516d961bf9
Tasha <39377219+snata97@users.noreply.github.com>
2020-07-01 10:09:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 require('dotenv').config();
 var express = require('express');
-var cors = require('cors')
+//var cors = require('cors')
 var bodyParser = require('body-parser');
 var database = require('./db');
 var db = database.connect();
 var app = express();
 const routes = require('./routes');
 //app.use(cors());
-app.options('*', cors({credentials: true, origin: true}));
-//app.use(function (req, res, next) {
-//    res.setHeader("Access-Control-Allow-Origin", "*");
-//    res.setHeader("Access-Control-Allow-Credentials", "true");
-//    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-//    res.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
-//    res.setHeader("Content-Type", "application/json");
-//    next();
-//});
+//app.options('*', cors({credentials: true, origin: true}));
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
+    res.setHeader("Content-Type", "application/json");
+    next();
+});
 app.use(bodyParser.json());//Парсить данные json
 app.use(bodyParser.urlencoded({extended: true}));//Парсить данные формы
 
