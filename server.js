9528e9477c0f70db954be30b9c5d8439dc4dbe51
Tasha <39377219+snata97@users.noreply.github.com>
2020-07-01 09:49:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ var database = require('./db');
 var db = database.connect();
 var app = express();
 const routes = require('./routes');
-// app.use(cors());
-// app.options('*', cors());
-app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
-    res.setHeader("Content-Type", "application/json");
-    next();
-});
+//app.use(cors());
+app.options('*', cors({credentials: true, origin: true}));
+//app.use(function (req, res, next) {
+//    res.setHeader("Access-Control-Allow-Origin", "*");
+//    res.setHeader("Access-Control-Allow-Credentials", "true");
+//    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+//    res.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
+//    res.setHeader("Content-Type", "application/json");
+//    next();
+//});
 app.use(bodyParser.json());//Парсить данные json
 app.use(bodyParser.urlencoded({extended: true}));//Парсить данные формы
 
