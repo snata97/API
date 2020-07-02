cca500b3f12fcbc859a3c2bacc85f1b83df22699
Tasha <39377219+snata97@users.noreply.github.com>
2020-07-02 10:48:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
+    res.setHeader("Content-Type", "application/json");
     //res.setHeader("Access-Control-Request-Method", "GET,POST,PUT,DELETE");
     //res.setHeader("Access-Control-Request-Headers", "Origin,X-Requested-With,Content-Type,Accept");
     next();
